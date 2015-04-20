a41e37eb462536057d59f04414f86bbd751e65c1
kkdawg <kdawg@OWNERs-MacBook-Pro.local>
2015-04-20 00:48:32 -0400

    basic functionality

--- shouldi.js
+++ shouldi.js
@@ -1,3 +1,5 @@
+window.onload = function() {
+
 var btn = document.querySelector( '.btn' );
 
 var btnFront = btn.querySelector( '.btn-front' ),
@@ -50,4 +52,6 @@ function distance( x1, y1, x2, y2 ) {
   var dx = x1-x2;
   var dy = y1-y2;
   return Math.sqrt( dx*dx + dy*dy );
+}
+
 }
